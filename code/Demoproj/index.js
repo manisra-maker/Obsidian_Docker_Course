022089d5cee202881bac7480bf911685522279cf
manisra-maker <manisra2317@gmail.com>
2025-08-07 15:29:16 +0530

    vault backup: 2025-08-07 15:29:16

--- code/Demoproj/index.js
+++ code/Demoproj/index.js
@@ -1,1 +1,11 @@
-const app = require('express');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+  if(req.url=='/'){
+    res.sendFile(__dirname+'index.html');
+  }
+})
