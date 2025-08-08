645ea0573b7a1ba79c1e576771275aeb554b669e
manisra-maker <manisra2317@gmail.com>
2025-08-08 13:05:45 +0530

    vault backup: 2025-08-08 13:05:45

--- code/Demoproj/public/index.js
+++ code/Demoproj/public/index.js
@@ -1,20 +1,22 @@
 var profile_flag = 1;
 
-document.getElementById("edit_profile").addEventListener("click", function() {
-  if (profile_flag == 1) {
-    console.log(`display is none`);
-    document.querySelectorAll("input").forEach((input) => {
-      input.style.display = "none"; // Hide the input
-      input.value = ""; // Clear the value
-      profile_flag = 0;
+document.getElementById("edit_profile").addEventListener("click", function () {
+  console.log(`Is getting called`);
+  const inputs = document.querySelectorAll("input");
+
+  if (profile_flag === 1) {
+    console.log("display is none");
+    inputs.forEach((input) => {
+      input.style.display = "none";
+      input.value = "";
     });
-  }
-  else{
-    console.log(`display is block`);
-    document.querySelectorAll("input").forEach((input) => {
-      input.style.display = "block"; // Hide the input
-      // input.value = ""; // Clear the value
-      profile_flag = 1;
+    profile_flag = 0;
+  } else {
+    console.log("display is block");
+    inputs.forEach((input) => {
+      input.style.display = "block";
     });
+    profile_flag = 1;
   }
 });
+
