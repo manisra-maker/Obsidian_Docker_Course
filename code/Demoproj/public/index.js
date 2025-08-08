14d0f449e4c664e7a22af3972d05ff33674de172
manisra-maker <manisra2317@gmail.com>
2025-08-08 12:43:41 +0530

    vault backup: 2025-08-08 12:43:41

--- code/Demoproj/public/index.js
+++ code/Demoproj/public/index.js
@@ -1,13 +1,20 @@
-var profile_flag =1;
+var profile_flag = 1;
 
-document.getElementById('edit_profile').addEventListener('click', function() {
-    if(profile_flag==1){
-
-  document.querySelectorAll('input').forEach(input => {
-    input.style.display = 'none';  // Hide the input
-    input.value = '';              // Clear the value
-    profile_flag=0;
-  });
-    }
+document.getElementById("edit_profile").addEventListener("click", function() {
+  if (profile_flag == 1) {
+    console.log(`display is none`);
+    document.querySelectorAll("input").forEach((input) => {
+      input.style.display = "none"; // Hide the input
+      input.value = ""; // Clear the value
+      profile_flag = 0;
+    });
+  }
+  else{
+    console.log(`display is block`);
+    document.querySelectorAll("input").forEach((input) => {
+      input.style.display = "block"; // Hide the input
+      // input.value = ""; // Clear the value
+      profile_flag = 1;
+    });
+  }
 });
-
