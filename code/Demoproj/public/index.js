441e3e6442d83431c59d585fd856f483e1424cea
manisra-maker <manisra2317@gmail.com>
2025-08-08 12:40:35 +0530

    vault backup: 2025-08-08 12:40:35

--- code/Demoproj/public/index.js
+++ code/Demoproj/public/index.js
@@ -1,9 +1,13 @@
 var profile_flag =1;
 
 document.getElementById('edit_profile').addEventListener('click', function() {
+    if(profile_flag==1){
+
   document.querySelectorAll('input').forEach(input => {
     input.style.display = 'none';  // Hide the input
     input.value = '';              // Clear the value
+    profile_flag=0;
   });
+    }
 });
 
