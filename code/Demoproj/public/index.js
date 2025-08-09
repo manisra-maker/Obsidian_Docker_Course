533d4f20928280d682d98f5d73f6da7e1bcf7a29
manisra-maker <manisra2317@gmail.com>
2025-08-09 19:04:08 +0530

    vault backup: 2025-08-09 19:04:08

--- code/Demoproj/public/index.js
+++ code/Demoproj/public/index.js
@@ -1,22 +1,30 @@
 var profile_flag = 1;
 
-document.getElementById("edit_profile").addEventListener("click", function () {
-  console.log(`Is getting called`);
-  const inputs = document.querySelectorAll("input");
+var button = document.getElementById("edit_profile");
+var name_field = document.getElementById("name");
+var name_span = document.createElement("span");
+var email_field = document.getElementById("email");
+var email_span = document.createElement("span");
+var intrest_field = document.getElementById("intrest");
+var intrest_span = document.createElement("span");
 
+document.getElementById("edit_profile").addEventListener("click", function() {
   if (profile_flag === 1) {
-    console.log("display is none");
-    inputs.forEach((input) => {
-      input.style.display = "none";
-      input.value = "";
-    });
+    name_span.innerText = name_field.value;
+    email_span.innerText = email_field.value;
+    intrest_span.innerText = intrest_field.value;
+    name_field.parentNode.replaceChild(name_span, name_field);
+    email_field.parentNode.replaceChild(email_span, email_field);
+    intrest_field.parentNode.replaceChild(intrest_span, intrest_field);
+
     profile_flag = 0;
+    button.innerHTML = "Edit Profile";
   } else {
-    console.log("display is block");
-    inputs.forEach((input) => {
-      input.style.display = "block";
-    });
+    name_span.parentNode.replaceChild(name_field, name_span);
+    email_span.parentNode.replaceChild(email_field, email_span);
+    intrest_span.parentNode.replaceChild(intrest_field, intrest_span);
+    button.innerHTML = "Save Profile";
+
     profile_flag = 1;
   }
 });
-
---
+++ notes/Docker_Network.md
@@ -0,0 +1,10 @@
+---
+id: Docker_Network
+aliases: []
+tags: []
+created on : 2025-08-09 18:01
+---
+
+# Docker network diagram of the NodeJS project
+
+![[../assets/imgs/Docker_Network.png]]
